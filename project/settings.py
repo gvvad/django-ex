6a2cf848d0cef605b4b3f35faf311a48f3e4a621
gvvad <gvvvad92@gmail.com>
2018-04-21 01:45:23 +0300

    com

--- project/settings.py
+++ project/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    #'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -77,7 +77,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'wsgi.application'
-CSRF_COOKIE_SECURE = True
+#CSRF_COOKIE_SECURE = True
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
